1c0eb73529c704dc42e7a157c3a728c1c47256c2
KumananEswaran <kumanan1731@gmail.com>
2025-06-27 16:02:59 +0800

    create function to add a new task

--- src/components/FormToAddTask.jsx
+++ src/components/FormToAddTask.jsx
@@ -1,7 +1,14 @@
 import { Form } from 'react-bootstrap';
 import CreatableSelect from 'react-select/creatable';
 
-const FormToAddTask = () => {
+const FormToAddTask = ({
+	title,
+	setTitle,
+	description,
+	setDescription,
+	status,
+	setStatus,
+}) => {
 	const options = [
 		{ value: 'new', label: 'New' },
 		{ value: 'in-progress', label: 'In-progress' },
@@ -10,27 +17,41 @@ const FormToAddTask = () => {
 
 	return (
 		<>
-			<Form.Group className="mb-3" controlId="task">
+			<Form.Group className="mb-3" controlId="title">
 				<Form.Label className="fw-semibold">Task Title</Form.Label>
-				<Form.Control type="text" placeholder="e.g. Submit project report" />
+				<Form.Control
+					type="text"
+					placeholder="e.g. Submit project report"
+					value={title}
+					onChange={(e) => setTitle(e.target.value)}
+					required
+				/>
 				<Form.Control.Feedback type="invalid">
 					Task title is required.
 				</Form.Control.Feedback>
 			</Form.Group>
-			<Form.Group className="mb-3" controlId="decription">
+			<Form.Group className="mb-3" controlId="description">
 				<Form.Label className="fw-semibold">Task Description</Form.Label>
 				<Form.Control
 					as="textarea"
 					rows={3}
 					placeholder="Briefly describe the task.."
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					required
 				/>
 				<Form.Control.Feedback type="invalid">
 					Task description is required.
 				</Form.Control.Feedback>
 			</Form.Group>
-			<Form.Group className="mb-3" controlId="tags">
+			<Form.Group className="mb-3" controlId="status">
 				<Form.Label className="fw-semibold">Task Status</Form.Label>
-				<CreatableSelect options={options} placeholder="Select a status" />
+				<CreatableSelect
+					options={options}
+					placeholder="Select a status"
+					value={status}
+					onChange={(selectedOption) => setStatus(selectedOption)}
+				/>
 			</Form.Group>
 		</>
 	);
--- src/pages/Addtasks.jsx
+++ src/pages/Addtasks.jsx
@@ -1,23 +1,81 @@
 import FormToAddTask from '../components/FormToAddTask';
 import NavigationBar from '../components/Navigationbar';
-import { Container, Form, Button } from 'react-bootstrap';
+import { Container, Form, Button, Spinner } from 'react-bootstrap';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Addtasks = () => {
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	const [status, setStatus] = useState({ value: 'new', label: 'New' });
+
+	const [validated, setValidated] = useState(false);
+
+	const [submitting, setSubmitting] = useState(false);
+
+	const navigate = useNavigate();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const form = e.currentTarget;
+		if (form.checkValidity() === false) {
+			e.stopPropagation();
+			setValidated(true);
+			return;
+		}
+		setSubmitting(true);
+		try {
+			await axios.post(
+				'https://taskmanagement-backend-ten.vercel.app/add-task',
+				{
+					title,
+					description,
+					status: status.value,
+				}
+			);
+			// toast.success('Contact saved successfully!');
+		} catch (error) {
+			console.error('Error saving contact:', error);
+			// toast.error('Failed to save contact. Please try again.');
+		}
+		setSubmitting(false);
+		return navigate('/tasks');
+	};
+
 	return (
 		<>
 			<NavigationBar />
 			<Container className="py-5">
-				<h2 className="mb-4 text-center fw-bold">Add a New Contact</h2>
+				<h2 className="mb-4 text-center fw-bold">Add a New Task</h2>
 				<Form
+					noValidate
+					validated={validated}
 					className="bg-white p-4 rounded shadow mx-auto"
-					style={{ maxWidth: '600px' }}>
+					style={{ maxWidth: '600px' }}
+					onSubmit={handleSubmit}>
 					<div className="d-grid">
-						<FormToAddTask />
+						<FormToAddTask
+							title={title}
+							setTitle={setTitle}
+							description={description}
+							setDescription={setDescription}
+							status={status}
+							setStatus={setStatus}
+						/>
 						<Button
 							className="submit-btn-indigo fw-semibold"
 							size="lg"
-							type="submit">
-							Add Task
+							type="submit"
+							disabled={submitting}>
+							{submitting ? (
+								<>
+									<Spinner size="sm" animation="border" className="me-2" />
+									Submitting...
+								</>
+							) : (
+								'Add Task'
+							)}
 						</Button>
 					</div>
 				</Form>
